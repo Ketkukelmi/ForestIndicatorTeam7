7f2c91b2d9b92b45323d02ed9cd670ab0d7126df
Ketkukelmi <t5mari00@students.oamk.fi>
2017-11-24 12:14:17 +0200

    MultipleSelector to own component

--- forest-indicator/src/components/rightwing.js
+++ forest-indicator/src/components/rightwing.js
@@ -1,64 +1,31 @@
-
 import React, { Component } from 'react'
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+import MultipleSelector from './MultipleSelector';
 
-const Choices = [
-    { label: 'Kantohinta-arvo', value: 'StubPriceValue'},
-    { label: 'Nettotulojen nykyarvo', value: 'NetProfitValue'},
-    { label: 'Hakkuukertymä', value: 'BeatingUp'},
-    { label: 'Tilavuus', value: 'Volume'}
-];
 
 
 class rightwing extends Component {
 
-    constructor(props) {
-        super(props)
-		this.state = {
-			removeSelected: true,
-			disabled: false,
-			stayOpen: false,
-			value: [],
-			rtl: false
-        };
-        
-        this.handleSelectChange = this.handleSelectChange.bind(this);
-    }
     
-	handleSelectChange (value) {
-		console.log('You\'ve selected:', value);
-		this.setState({ value });
-	}
-
-    
-
-    state = {
-        selectedOption: '',
-      }
-      handleChange = (selectedOption) => {
-        this.setState({ selectedOption });
-        console.log(`Selected: ${selectedOption.label}`);
-      }
 
     render () {
-        const { stayOpen, value } = this.state;
-        const options = Choices;
+
+        let choices = [
+            { label: 'Kantohinta-arvo', value: 'StubPriceValue'},
+            { label: 'Nettotulojen nykyarvo', value: 'NetProfitValue'},
+            { label: 'Hakkuukertymä', value: 'BeatingUp'},
+            { label: 'Tilavuus', value: 'Volume'},
+        ];
+        let Gatherings = [
+            {label: 'Mustikkasato', value: "BlueberryHarvest"},
+            {label: 'Puolukkasato', value: "LingonHarvest"},
+        ];
+        const title = "Puutuotanto";
+        
         return (
             <div class="col-md-3">
-            <p>Puutuotanto</p>
-            <Select
-					closeOnSelect={!stayOpen}
-					multi
-                    onChange={this.handleSelectChange}
-                    options={options}
-					placeholder="Valitse yksi tai useampi vaihtoehto"
-                    removeSelected={this.state.removeSelected}
-					rtl={this.state.rtl}
-					simpleValue
-					value={value}
-				/>
-
+            <MultipleSelector choices = {choices} title = {title} />
             </div>
         )
     }
