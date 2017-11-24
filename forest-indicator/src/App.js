5c707da71a52cbb7d13943ea5b8bf13cdf8eb877
milla juntunen <t5haje00@students.oamk.fi>
2017-11-24 09:30:22 +0200

    ui start

--- forest-indicator/src/App.js
+++ forest-indicator/src/App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+<div className="container">
+
+<div class="row">
+
+<div class="col-md-12 text-center">
+<h1>OTSIKKO</h1>
+</div>
+
+</div>
+
+<div class="row">
+
+<div class="col-md-3">
+<ul>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+</ul>
+</div>
+
+<div class="col-md-6">
+<img src="http://www.joshuacasper.com/contents/uploads/joshua-casper-samples-free.jpg"/>
+</div>
+
+<div class="col-md-3">
+<ul>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+<li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+  <li>Lista asia</li>
+</ul>
+
+</div>
+</div>
+</div>
     );
   }
 }
