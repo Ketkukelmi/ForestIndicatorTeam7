ee1840bee55dc52a38956a487f63626c04968dd0
milla juntunen <t5haje00@students.oamk.fi>
2017-11-24 10:14:59 +0200

    sample paint kuva

--- forest-indicator/src/components/middlewing.js
+++ forest-indicator/src/components/middlewing.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import testimg from './test.png';
 
 class middlewing extends Component {
     render () {
         return (
-<div class="col-md-6">
-<img src="http://www.joshuacasper.com/contents/uploads/joshua-casper-samples-free.jpg"/>
-</div>
+    <div class="col-md-6">
+        <h2 className="text-center">Alue</h2>
+
+    <figure className="text-center" >
+        <h4>Skenaario</h4>
+        <img src={testimg} alt="logo" /> 
+        <figcaption >Lähde</figcaption>
+        <br/>
+    </figure>
+
+    <figure className="text-center" >
+        <h4>Skenaario</h4>
+        <img src={testimg} alt="logo" /> 
+        <figcaption >Lähde</figcaption>
+        <br/>
+    </figure>
+
+    <figure className="text-center" >
+        <h4>Skenaario</h4>
+        <img src={testimg} alt="logo" /> 
+        <figcaption >Lähde</figcaption>
+        <br/>
+    </figure>
+     
+        <br/>
+        <button type="button" className="btn btn-primary">Kaavio 1</button>   
+        <button type="button" className="btn btn-primary">Kaavio 2</button>   
+        <button type="button" className="btn btn-primary">Taulukko</button>   
+        <button type="button" className="btn btn-primary pull-right">Lataa</button>   
+        <button type="button" className="btn btn-primary pull-right">Printtaa</button>   
+        </div>
         )
     }
 }
