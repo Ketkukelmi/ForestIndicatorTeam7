e7111a4f34127968b930bb6b22330344d85e0c86
Janne Turunen <t5tuja02@students.oamk.fi>
2017-11-28 08:33:25 +0200

    Added simple dropdown

--- forest-indicator/src/App.js
+++ forest-indicator/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/header.js';
-import Leftwing from './components/leftwing.js';
+import LeftView from './components/LeftView.js';
 import Middlewing from './components/middlewing.js';
 import Rightwing from './components/rightwing.js';
 
---
+++ forest-indicator/src/components/SimpleDropDown.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+class SimpleDropDown extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedOption: '',
+      label: 'Alueet:',
+      searchable: true,
+      clearable: true,
+    }
+
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue(newValue) {
+    this.setState({
+      selectValue: newValue,
+    });
+  }
+
+  render () {
+
+
+    return (
+      <div>
+        <p>{this.props.title}</p>
+        <Select
+          id="area-select"
+          options={this.props.options}
+          simpleValue
+          clearable={this.state.clearable}
+          name="selected-area"
+          value={this.state.selectValue}
+          onChange={this.updateValue}
+          openOnClick={false}
+          searchable={this.state.searchable}
+        />
+      </div>
+    )
+  }
+}
+
+export default SimpleDropDown
