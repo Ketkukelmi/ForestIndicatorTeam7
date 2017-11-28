a694ba08294c0d5d14f58bf2ffca848abd949db6
Ketkukelmi <t5mari00@students.oamk.fi>
2017-11-28 08:54:59 +0200

    Multiselector dataparts

--- forest-indicator/src/components/rightview.js
+++ forest-indicator/src/components/rightview.js
@@ -17,15 +17,40 @@ class rightwing extends Component {
             { label: 'Hakkuukertymä', value: 'BeatingUp'},
             { label: 'Tilavuus', value: 'Volume'},
         ];
-        let Gatherings = [
+
+        let gatherings = [
             {label: 'Mustikkasato', value: "BlueberryHarvest"},
             {label: 'Puolukkasato', value: "LingonHarvest"},
         ];
+
+        let diversity = [
+            {label: 'Lahopuun määrä', value: "MoldTree"},
+            {label: 'Putkilokasvien lkm', value: 'Pipe'},
+            {label: 'Mustikan peittävyys', value: 'BlueberryHidings'},
+            {label: 'Jäkälien peittävyys', value: 'JakalaHidings'},
+            {label: 'Käävät', value: 'oldPeople'},
+        ];
+
+        let coal =[{label: "Hiilen määrä", value: 'coalAmount'}];
+        let rest =[{label: "Biomassa", value: 'BioMass'}];
+
         const title = "Puutuotanto";
+        const title1 = "Keruutuotteet";
+        const title2 = "Monimuotoisuus";
+        const title3 = "Hiilen määrä";
+        const title4 = "Biomassa";
         
         return (
             <div class="col-md-3">
             <MultipleSelector choices = {choices} title = {title} />
+            <br></br>
+            <MultipleSelector choices = {gatherings} title = {title1} />
+            <br></br>
+            <MultipleSelector choices = {diversity} title = {title2} />
+            <br></br>
+            <MultipleSelector choices = {coal} title = {title3} />
+            <br></br>
+            <MultipleSelector choices = {rest} title = {title4} />
             </div>
         )
     }
